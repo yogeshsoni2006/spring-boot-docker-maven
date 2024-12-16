98edafc89d67c48caccaecff3e5878ff9127a945
yogeshsoni2006 <yogeshsoni2006@gmail.com>
2024-12-16 20:06:27 +0530

    docker with maven

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	@GetMapping("/")
+	public String demo() {
+		return "Welcome my first AWS Demo";
+	}
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to first docker";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
